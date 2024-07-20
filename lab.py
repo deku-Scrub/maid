428e636b85c7da00a1a4af7d912820a06b7982cc
deku scrub <@.@>
2024-07-20 04:27:07 +0000

    Add experimentation file

--- lab.py
+++ lab.py
@@ -1,57 +1,82 @@
-import maid.graph
+import maid
 
+main_maid = maid.get_maid()
+main_maid.add_task(
+        'xa',
+        lambda i, o, t, mit: print('xa'),
+        targets=[],
+        required_tasks=[],
+        required_files=[],
+        run_at_start=True,
+        run_at_end=True,
+        )
+main_maid.add_task(
+        'x0',
+        lambda rf, rt, t, mrt: f'touch {t[0]}',
+        targets=['0'],
+        required_tasks=[],
+        required_files=[],
+        )
+main_maid.add_task(
+        'x1',
+        lambda rf, rt, t, mrt: f'touch {t[0]}',
+        targets=['1'],
+        required_tasks=[],
+        required_files=[],
+        #always_run_if_in_graph=True,
+        )
+main_maid.add_task(
+        'x2',
+        lambda rf, rt, t, mrt: f'touch {t[0]}',
+        targets=['2'],
+        required_tasks=['x1'],
+        required_files=[],
+        )
+main_maid.add_task(
+        'x3',
+        lambda rf, rt, t, mrt: f'touch {t[0]}',
+        targets=['3'],
+        required_tasks=['x2'],
+        required_files=[],
+        )
+main_maid.add_task(
+        'x4',
+        lambda rf, rt, t, mrt: f'touch {t[0]}',
+        targets=['4'],
+        required_tasks=['x2'],
+        required_files=[],
+        #dont_run_if_all_targets_exist=True,
+        )
+main_maid.add_task(
+        'x5',
+        lambda rf, rt, t, mrt: (print(mrt), f'touch {t[0]}')[1],
+        targets=['5'],
+        required_tasks=['x3', 'x4'],
+        required_files=[],
+        )
+main_maid.add_task(
+        'x6',
+        lambda rf, rt, t, mrt: f'touch {t[0]}',
+        targets=['6'],
+        required_tasks=['x1', 'x5', 'x7'],
+        required_files=[],
+        is_default=True,
+        )
+main_maid.add_task(
+        'x7',
+        lambda rf, rt, t, mrt: f'touch {t[0]}; echo lalala',
+        #lambda rf, rt, t, mrt: f'touch {t[0]}; lalala',
+        #lambda rf, rt, t, mrt: 'lol'.replace('l', 'o') if False else False,
+        #lambda rf, rt, t, mrt: print('lalal'),
+        targets=['7'],
+        required_tasks=['x0'],
+        required_files=[],
+        #delete_targets_on_error=False,
+        print_script=False,
+        print_script_output=True,
+        #shell=('python3', '-c'),
+        )
 
-def get_deps(inputs, outputs):
-    '''
-    '''
-    return f'python3 -m pip install -t deps -r {inputs[0]} && touch deps'
-
-
-task_graph = {
-    'mkdir': {
-        'inputs': ('maid',),
-        'outputs': ('mkdir',),
-        'function': lambda i, o: f'touch {o[0]}',
-    },
-    'get_deps': {
-        'inputs': ('requirements.txt',),
-        'outputs': ('deps',),
-        'function': get_deps,
-    },
-    'download': {
-        'inputs': ('get_deps',),
-        'outputs': ('download',),
-        'function': lambda i, o: f'touch {o[0]}',
-    },
-    'size': {
-        'inputs': ('download',),
-        'outputs': ('size',),
-        'function': lambda i, o: f'touch {o[0]}',
-    },
-    'count': {
-        'inputs': ('download', 'mkdir'),
-        'outputs': ('count',),
-        'function': lambda i, o: f'touch {o[0]}',
-    },
-    'find': {
-        'inputs': ('size',),
-        'outputs': ('find',),
-        'function': lambda i, o: f'touch {o[0]}',
-    },
-    'synonyms': {
-        'inputs': ('size', 'count'),
-        'outputs': ('synonyms',),
-        'function': lambda i, o: f'touch {o[0]}',
-    },
-    'db': {
-        'inputs': ('synonyms', 'download'),
-        'outputs': ('db',),
-        'function': lambda i, o: f'touch {o[0]}',
-    },
-}
-
-graph = maid.graph.Graph()
-graph.update(task_graph)
-graph.dry_run('help')
-#graph.dry_run('db')
-#graph.run('db')
+#main_maid.describe_goals()
+#main_maid.describe_tasks()
+#main_maid.work('x6', dry_run=True)
