c1213f5a3042d99573762d8fe027e6d630c7c5d6
deku scrub <@.@>
2024-07-26 21:21:59 +0000

    Move SimpleTask to maid.composition

--- maid/composition.py
+++ maid/composition.py
@@ -1,5 +1,9 @@
 import itertools
+import functools
+import sys
 import subprocess
+
+import maid.exceptions
 
 
 class ShellPipeline:
@@ -43,3 +47,121 @@ class ShellPipeline:
 
         # Yield output of last command.
         yield from processes[-1].stdout
+
+
+class SimpleTask:
+
+    def __init__(
+            self,
+            *,
+            inputs=tuple(),
+            script_stream=sys.stdout,
+            output_stream=sys.stdout,
+            ):
+        '''
+        '''
+        self._inputs = inputs
+        self._commands = []
+        self._outfile = ''
+        self._mode = ''
+        self._script_stream = script_stream
+        self._output_stream = output_stream
+
+    def append(self, command):
+        '''
+        '''
+        match command:
+            case str():
+                match self._commands:
+                    case []:
+                        self._commands.append(ShellPipeline())
+                    case list(x) if not isinstance(x[-1], ShellPipeline):
+                        self._commands.append(ShellPipeline())
+                self._commands[-1].append(command)
+            case tuple():
+                self._commands.append(command)
+            case x if callable(x):
+                self._commands.append(command)
+            case _:
+                raise maid.exceptions.UnknownCommandTypeException(command)
+
+    def write_to_file(self, filename):
+        SimpleTask._validate_filename(filename)
+        self._outfile = filename
+        self._mode = 'wt'
+
+    def append_to_file(self, filename):
+        SimpleTask._validate_filename(filename)
+        self._outfile = filename
+        self._mode = 'at'
+
+    def _validate_filename(filename):
+        #if self._outfile:
+            #raise maid.exceptions.EndOfTaskError(filename)
+        if not isinstance(filename, str):
+            raise maid.exceptions.InvalidFileTypeException(filename)
+        if not filename:
+            raise maid.exceptions.EmptyOutputFileException()
+
+    def __str__(self):
+        '''
+        Return a string representation of this object's commands.
+        '''
+        commands = '\n'.join(map(str, self._commands)).replace('\n', '\n    | ')
+        append = f'\n    >> {self._outfile}' if self._mode == 'at' else ''
+        truncate = f'\n    > {self._outfile}' if self._mode == 'wt' else ''
+        return '{commands}{truncate}{append}'.format(
+                commands=commands,
+                append=append,
+                truncate=truncate,
+                )
+
+    def run(self):
+        outputs = functools.reduce(
+                self._run_command,
+                self._commands,
+                self._inputs,
+                )
+        self._postrun(outputs)
+        return outputs
+
+    def _run_command(self, inputs, command):
+        _print_scripts(self._script_stream, command)
+        match command:
+            case ShellPipeline():
+                return command(inputs)
+            case tuple():
+                return command[0](*command[1:], inputs)
+            case _ if callable(command):
+                return map(command, inputs)
+            case _:
+                raise maid.exceptions.UnknownCommandTypeException(command)
+
+    def _postrun(self, outputs):
+        '''
+        Run functions that require the task to have finished.
+        '''
+        if self._outfile:
+            _print_scripts(
+                    self._script_stream,
+                    '{mode} {file}\n'.format(
+                        mode='>' if self._mode.startswith('w') else '>>',
+                        file=self._outfile,
+                        ))
+        if not _write_to_file(outputs, self._outfile, self._mode):
+            if self._output_stream:
+                self._output_stream.writelines(outputs)
+
+
+def _print_scripts(outstream, command):
+    if outstream:
+        outstream.write(str(command) + '\n')
+
+
+def _write_to_file(lines, filename, mode):
+    if not filename:
+        return False
+
+    with open(filename, mode=mode) as fos:
+        fos.writelines(lines)
+    return True
