55a98d9f8597831ba21df12f28a5a0a9dc5e1915
deku scrub <@.@>
2024-08-11 19:01:18 -0400

    Require task name when using decorator

--- maid/decorators.py
+++ maid/decorators.py
@@ -8,10 +8,12 @@ type RecipeBuilder = Callable[[maid.tasks.Task], maid.compose.Recipe]
 
 
 def task(
+        name: str,
+        /,
         **kwargs: Any,
         ) -> Callable[[RecipeBuilder], maid.tasks.Task]:
     def f(g: RecipeBuilder) -> maid.tasks.Task:
-        t = maid.tasks.Task(g, **kwargs)
+        t = maid.tasks.Task(g, name=name, **kwargs)
         maid.maids.get_maid().add_task(t)
         return t
     return f
