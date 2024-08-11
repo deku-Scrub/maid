6d1eff8828f7bca02ba7c32ed4f0ac08fe7c223c
deku scrub <@.@>
2024-08-11 12:05:38 -0400

    Simplify decorators

--- maid/decorators.py
+++ maid/decorators.py
@@ -1,46 +1,17 @@
-from typing import IO, Callable, Sequence, Any
-import sys
+from typing import Any, Callable
 
-import maid.maid_utils
-import maid.cache
-import maid.compose.tasks
+import maid2.tasks
+import maid2.compose
+import maid2.maids
 
-type TaskBuilder = Callable[[], maid.compose.tasks.Task]
-type CommandDefiner = Callable[[maid.compose.tasks.Task], None]
+type RecipeBuilder = Callable[[maid2.tasks.Task], maid2.compose.Recipe]
 
 
 def task(
-        name: str,
-        maid_name: str = maid.maid_utils.DEFAULT_MAID_NAME,
-        inputs: Sequence[Any] = tuple(),
-        required_files: Sequence[str] = tuple(),
-        required_tasks: Sequence[TaskBuilder] = tuple(),
-        targets: Sequence[str] = tuple(),
-        cache: maid.cache.CacheType = maid.cache.CacheType.NONE,
-        output_stream: IO = sys.stdout,
-        script_stream: IO = sys.stderr,
-        independent_targets: bool = False,
-        is_default: bool = False,
-        ) -> Callable[[CommandDefiner], TaskBuilder]:
-
-    def build_task(define_commands: CommandDefiner) -> TaskBuilder:
-        t = maid.compose.tasks.Task(
-                name,
-                maid_name=maid_name,
-                inputs=inputs,
-                required_files=required_files,
-                targets=targets,
-                cache=cache,
-                is_default=is_default,
-                build_task=define_commands if independent_targets else None,
-                required_tasks=required_tasks,
-                output_stream=output_stream,
-                script_stream=script_stream,
-                )
-        # Let `define_commands` immediately create commands for the task.
-        define_commands(t)
-        _ = maid.maid_utils.get_maid(maid_name=maid_name).add_task(t)
-
-        return lambda: t
-
-    return build_task
+        **kwargs: Any,
+        ) -> Callable[[RecipeBuilder], maid2.tasks.Task]:
+    def f(g: RecipeBuilder) -> maid2.tasks.Task:
+        t = maid2.tasks.Task(g, **kwargs)
+        maid2.maids.get_maid().add_task(t)
+        return t
+    return f
